528193666032ce93f9ed52c966a8dac5bf0d53ac
HMPrgm <henrymprof@gmail.com>
2026-04-26 15:54:43 -0400

    simple store

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
+
+type Data struct {
+	Val int `json:"data"`
+}
 
 func main() {
 	port := ":8080"
+	
+	var x int = 0
 	fmt.Printf("Starting storage node on port %s...\n", port)
 
-	http.HandleFunc("/bw", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Nett hier. Aber waren Sie schon mal in Baden-Württemberg?"))
+	router := gin.Default()
+	router.GET("/ping", func (c *gin.Context)  {
+		c.JSON(200, gin.H{
+			"message": "Nett hier, aber waren sie schon mal in Baden-Wuttemberg?",
+		})
 	})
 
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+	router.GET("/", func (c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": x,
+		})
+	})
+
+	router.POST("/", func(c *gin.Context) {
+		var jsonBody Data
+
+		if err := c.ShouldBindJSON(&jsonBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		x = jsonBody.Val
+		c.JSON(200, gin.H{
+			"data": x,
+		})
+	})
+
+	router.DELETE("/", func(c *gin.Context) {
+		x = 0
+		c.JSON(http.StatusOK, gin.H{
+			"data": x,
+		})
+	})
+	router.Run()
 }
